434600f3d8c8d2501cc1aab2a251a059e698b9cc
sahartvk <sahartavakoli2002@gmail.com>
2022-06-07 01:22:24 +0430

    get+set+inc+clr+a bit of reset

--- mano_simulator/mainwindow.cpp
+++ mano_simulator/mainwindow.cpp
@@ -20,6 +20,72 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//quint16 : Typedef for unsigned short. This type is guaranteed to be 16-bit on all platforms supported by Qt.
+quint16 MainWindow::getE() { return E.to_ulong(); }
+quint16 MainWindow::getDR() { return DR.to_ulong(); }
+quint16 MainWindow::getAC() { return AC.to_ulong(); }
+bool MainWindow::getACleadingBit() { return AC[15]; }
+quint16 MainWindow::getIR() { return IR.to_ulong(); }
+quint16 MainWindow::getTR() { return TR.to_ulong(); }
+quint16 MainWindow::getAR() { return AR.to_ulong(); }
+quint16 MainWindow::getPC() { return PC.to_ulong(); }
+quint16 MainWindow::getINPR() { return INPR.to_ulong(); }
+quint16 MainWindow::getOUTR() { return OUTR.to_ulong(); }
+
+
+void MainWindow::setE(const quint16& E) { this->E = std::bitset<1>(E); }
+void MainWindow::setDR(const quint16& DR) { this->DR = std::bitset<16>(DR); }
+void MainWindow::setAC(const quint16& AC) { this->AC = std::bitset<16>(AC); }
+void MainWindow::setIR(const quint16& IR) { this->IR = std::bitset<16>(IR); }
+void MainWindow::setTR(const quint16& TR) { this->TR = std::bitset<16>(TR); }
+void MainWindow::setAR(const quint16& AR) { this->AR = std::bitset<12>(AR); }
+void MainWindow::setPC(const quint16& PC) { this->PC = std::bitset<12>(PC); }
+void MainWindow::setINPR(const quint8& INPR) { this->INPR = std::bitset<8>(INPR); }
+void MainWindow::setOUTR(const quint8& OUTR) { this->OUTR = std::bitset<8>(OUTR); }
+
+//edit inr to inc
+void MainWindow::incAR() { setAR(getAR() + 1); }
+void MainWindow::incPC() { setPC(getPC() + 1); }
+void MainWindow::incDR() { setDR(getDR() + 1); }
+void MainWindow::incAC() { setAC(getAC() + 1); }
+void MainWindow::incTR() { setTR(getTR() + 1); }
+
+void MainWindow::clrAR() { AR.reset(); }
+void MainWindow::clrPC() { PC.reset(); }
+void MainWindow::clrDR() { DR.reset(); }
+void MainWindow::clrAC() { AC.reset(); }
+void MainWindow::clrTR() { TR.reset(); }
+
+void MainWindow::logicUnitAND(){ AC = AC & DR; }
+void MainWindow::logicUnitCMA() { AC = ~AC; }
+
+//??
+void MainWindow::emptyTable()
+{
+    for(int i=0;i<4096;i++)
+    {
+        //number : Returns a string equivalent of the number n according to the specified base.
+        QString result = QString::number( i, 16 ).toUpper();
+        //??
+        QTableWidgetItem *itm = new QTableWidgetItem();
+        QTableWidgetItem *empty = new QTableWidgetItem();
+        itm->setText(result);
+        empty->setText("0000");
+        //ram_tb->RAM
+        ui->RAM->insertRow(i);
+        ui->RAM->setItem(i,1,itm);
+        ui->RAM->setItem(i,3,empty);
+
+    }
+}
+
+void MainWindow::resetRam()
+{
+    for(int i=0;i<4096;i++)
+    {
+        ram[i].reset();
+    }
+}
 
 void MainWindow::on_actionNew_triggered()
 {
--- mano_simulator/mainwindow.h
+++ mano_simulator/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<bitset>
+#include<map>
+
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +18,50 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    //quint16 : Typedef for unsigned short. This type is guaranteed to be 16-bit on all platforms supported by Qt.
+    quint16 getE();
+    quint16 getDR();
+    quint16 getAC();
+    bool getACleadingBit();
+    quint16 getIR();
+    quint16 getTR();
+    quint16 getAR();
+    quint16 getPC();
+    quint16 getINPR();
+    quint16 getOUTR();
+
+
+    void setE(const quint16& E);
+    void setDR(const quint16& DR);
+    void setAC(const quint16& AC);
+    void setIR(const quint16& IR);
+    void setTR(const quint16& TR);
+    void setAR(const quint16& AR);
+    void setPC(const quint16& PC);
+    void setINPR(const quint8& INPR);
+    void setOUTR(const quint8& OUTR);
+
+
+    //edit inr to inc
+    void incAR();
+    void incPC();
+    void incDR();
+    void incAC();
+    void incTR();
+
+    void clrAR();
+    void clrPC();
+    void clrDR();
+    void clrAC();
+    void clrTR();
+
+    void logicUnitAND();
+    void logicUnitCMA();
+
+    void emptyTable();
+    void resetRam();
+
 
 private slots:
     void on_actionNew_triggered();
@@ -28,7 +76,48 @@ private slots:
 
     void on_actionRun_triggered();
 
+
+
+
 private:
     Ui::MainWindow *ui;
+
+
+    QString issaved="";
+    bitset<16> ram[4096];
+    bitset<16> DR;
+    bitset<16> SC;
+    bitset<16> AC;
+    bitset<16> IR;
+    bitset<16> TR;
+    bitset<12> AR;
+    bitset<12> PC;
+    bitset<8> INPR;
+    bitset<8> OUTR;
+    bitset<1> E;
+    bitset<1> I;
+    bitset<1> S;
+    bitset<1> R;
+    bitset<1> IEN;
+    bitset<1> FGI;
+    bitset<1> FGO;
+    map<QString,int> allDatas;
+    map<QString,int> firstallDatas;
+    QStringList commands;
+    int tcommmands;
+    int clk;
+    int lineStep;
+    int memorystep;
+
+
+    int reseter=1;
+    int compiled=0;
+    int printing;
+    int run;
+    map<int,int> memoryToLine;
+
+
+
+
 };
 #endif // MAINWINDOW_H
